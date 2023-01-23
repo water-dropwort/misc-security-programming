fb996c44bb0b5d42d370191f0f222133d6db4b88
water-dropwort <mizunoya.d@gmail.com>
2023-01-23 20:17:22 +0900

    fixed bug for pic_carver.py, and add test script for face_detection

--- chapter4/pic_carver.py
+++ chapter4/pic_carver.py
@@ -49,8 +49,8 @@ def extract_image(headers, http_payload):
 
 def face_detect(path, file_name):
     img     = cv2.imread(path)
-    cascade = cv2.CascadeCassifier("haarcascade_frontalface_alt.xml")
-    rects   = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    rects   = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20,20))
 
     if len(rects) == 0:
         return False
@@ -59,7 +59,7 @@ def face_detect(path, file_name):
 
     # 画像中のすべての顔をハイライト
     for x1,y1,x2,y2 in rects:
-        cv2.rectangle(img, (x1,y1), (x,y2), (127,255,0), 2)
+        cv2.rectangle(img, (x1,y1), (x2,y2), (127,255,0), 2)
 
     cv2.imwrite("%s/%s-%s" % (faces_directory, pcap_file, file_name), img)
 
